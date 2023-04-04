9d082969567fb341a3a8c95b9e9e9afbbe2d0bfa
claudiumt <claudium.tudor@gmail.com>
2023-04-05 02:55:48 +0300

    base Employee rest controller implementation (WIP)

--- src/main/java/com/codecool/seamanager/controller/EmployeeController.java
+++ src/main/java/com/codecool/seamanager/controller/EmployeeController.java
@@ -1,4 +1,31 @@
 package com.codecool.seamanager.controller;
 
+import com.codecool.seamanager.model.employee.Employee;
+import com.codecool.seamanager.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/employee")
 public class EmployeeController {
+
+	private final EmployeeService employeeService;
+
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@GetMapping
+	public List<Employee> getEmployees() {
+		return employeeService.getEmployees();
+
+	}
+
+	@PostMapping()
+	public void registerNewEmployee(@RequestBody Employee employee) {
+		employeeService.addNewEmployee(employee);
+	}
 }
