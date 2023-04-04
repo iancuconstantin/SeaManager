dc765a4fb2305dacca78126bb6392687db060955
claudiumt <claudium.tudor@gmail.com>
2023-04-05 02:50:58 +0300

    refactor Employee model with JPA annotations and new fields

--- src/main/java/com/codecool/seamanager/model/employee/Employee.java
+++ src/main/java/com/codecool/seamanager/model/employee/Employee.java
@@ -1,30 +1,53 @@
 package com.codecool.seamanager.model.employee;
 
 import com.codecool.seamanager.model.certificate.Certificate;
+import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
 
+@Entity
+@Table
 public class Employee {
-	private final String id;
-	private final String firstName;
-	private final String lastName;
-	private final String birthDate;
-	private final String address;
-	private Rank rank;
-	private Gender gender;
-	private final List<Certificate> certificates;
+	@Id
+	@GeneratedValue(
+			strategy = GenerationType.AUTO
+	)
+	private Long id;
+	@Column(name = "first_name")
+	private String a_firstName;
+	@Column(name = "last_name")
+	private String b_lastName;
+	@Column(name = "email")
+	private String c_email;
+	@Column(name = "birth_date")
+	private String d_birthDate;
+	@Column(name = "address")
+	private String e_address;
+	@Column(name = "contact_no")
+	private String f_contactNo;
+	@Column(name = "rank")
+	@Enumerated(EnumType.STRING)
+	private Rank g_rank;
+	@Column(name = "gender")
+	@Enumerated(EnumType.STRING)
+	private Gender h_gender;
+	@OneToMany(targetEntity = Certificate.class,
+	cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+	private List<Certificate> certificates;
 
-	public Employee(String firstName, String lastName, String birthDate, String address, Rank rank, Gender gender) {
-		this.id = UUID.randomUUID().toString();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.gender = gender;
-		this.birthDate = birthDate;
-		this.address = address;
-		this.rank = rank;
+	public Employee() {
+	}
+
+	public Employee(String firstName, String lastName, String birthDate, String contactNo, String address, String email, Rank rank, Gender gender) {
+		this.a_firstName = firstName;
+		this.b_lastName = lastName;
+		this.c_email = email;
+		this.d_birthDate = birthDate;
+		this.e_address = address;
+		this.f_contactNo = contactNo;
+		this.g_rank = rank;
+		this.h_gender = gender;
 		this.certificates = new ArrayList<>();
 	}
 
@@ -32,64 +55,62 @@ public class Employee {
 		certificates.add(certificate);
 	}
 
-	public List<Certificate> getCertificates() {
-		return certificates;
-	}
-
-	public Rank getRank() {
-		return rank;
-	}
-
-	public void setRank(Rank rank) {
-		this.rank = rank;
-	}
-
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
 	public String getFirstName() {
-		return firstName;
+		return a_firstName;
 	}
 
 	public String getLastName() {
-		return lastName;
+		return b_lastName;
+	}
+
+	public String getEmail() {
+		return c_email;
 	}
 
 	public String getBirthDate() {
-		return birthDate;
-	}
-
-	public Gender getGender() {
-		return gender;
+		return d_birthDate;
 	}
 
 	public String getAddress() {
-		return address;
+		return e_address;
+	}
+
+	public String getContactNo() {
+		return f_contactNo;
+	}
+
+	public Rank getRank() {
+		return g_rank;
+	}
+
+	public Gender getGender() {
+		return h_gender;
+	}
+
+	public List<Certificate> getCertificates() {
+		return certificates;
+	}
+
+	public void setRank(Rank rank) {
+		this.g_rank = rank;
 	}
 
 	@Override
 	public String toString() {
 		return "Employee{" +
-				"id='" + id + '\'' +
-				", firstName='" + firstName + '\'' +
-				", lastName='" + lastName + '\'' +
-				", birthDate='" + birthDate + '\'' +
-				", address='" + address + '\'' +
-				", certificates=" + certificates +
+				"id=" + id +
+				", firstName='" + a_firstName + '\'' +
+				", lastName='" + b_lastName + '\'' +
+				", email='" + c_email + '\'' +
+				", birthDate='" + d_birthDate + '\'' +
+				", address='" + e_address + '\'' +
+				", contactNo='" + f_contactNo + '\'' +
+				", rank=" + g_rank +
+				", gender=" + h_gender +
 				'}';
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Employee employee = (Employee) o;
-		return id.equals(employee.id) && firstName.equals(employee.firstName) && lastName.equals(employee.lastName) && birthDate.equals(employee.birthDate) && address.equals(employee.address);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, firstName, lastName, birthDate, address);
 	}
 }
