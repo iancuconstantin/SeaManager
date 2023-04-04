c57714f794c7ad23fe665b0f25f179c30a624031
claudiumt <claudium.tudor@gmail.com>
2023-04-04 13:39:30 +0300

    changed owner identifier from Employee to String id

--- src/main/java/com/codecool/seamanager/model/Certificate.java
+++ src/main/java/com/codecool/seamanager/model/Certificate.java
@@ -3,22 +3,22 @@ package com.codecool.seamanager.model;
 import java.util.Objects;
 
 public class Certificate {
-	private final Employee owner; //TODO - keep field??
+	private final String ownerID;
 	private final String description;
 	private final int serialNumber;
 	private String issueDate;
 	private String expiryDate;
 
-	public Certificate(Employee owner, String description, int serialNumber, String issueDate, String expiryDate) {
-		this.owner = owner;
+	public Certificate(String ownerID, String description, int serialNumber, String issueDate, String expiryDate) {
+		this.ownerID = ownerID;
 		this.description = description;
 		this.serialNumber = serialNumber;
 		this.issueDate = issueDate;
 		this.expiryDate = expiryDate;
 	}
 
-	public Employee getOwner() {
-		return owner;
+	public String getOwnerID() {
+		return ownerID;
 	}
 
 	public String getDescription() {
@@ -50,18 +50,18 @@ public class Certificate {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		Certificate that = (Certificate) o;
-		return serialNumber == that.serialNumber && owner.equals(that.owner) && description.equals(that.description) && issueDate.equals(that.issueDate) && Objects.equals(expiryDate, that.expiryDate);
+		return serialNumber == that.serialNumber && ownerID.equals(that.ownerID) && description.equals(that.description) && issueDate.equals(that.issueDate) && Objects.equals(expiryDate, that.expiryDate);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(owner, description, serialNumber, issueDate, expiryDate);
+		return Objects.hash(ownerID, description, serialNumber, issueDate, expiryDate);
 	}
 
 	@Override
 	public String toString() {
 		return "Certificate{" +
-				"owner=" + owner +
+				"ownerID=" + ownerID +
 				", description='" + description + '\'' +
 				", serialNumber=" + serialNumber +
 				", issueDate='" + issueDate + '\'' +
