8ac0bbbccb93cfebd564eca600dfe8a50dfa4766
claudiumt <claudium.tudor@gmail.com>
2023-04-05 02:57:05 +0300

    base EmployeeService implementation using JPA (WIP)

--- src/main/java/com/codecool/seamanager/service/EmployeeService.java
+++ src/main/java/com/codecool/seamanager/service/EmployeeService.java
@@ -1,4 +1,34 @@
 package com.codecool.seamanager.service;
 
+import com.codecool.seamanager.model.employee.Employee;
+import com.codecool.seamanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EmployeeService {
+	private final EmployeeRepository employeeRepository;
+
+	@Autowired
+	public EmployeeService(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+	public List<Employee> getEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	public void addNewEmployee(Employee employee) {
+		System.out.println(employee);
+		Optional<Employee> employeeOptional = employeeRepository.findEmployeeByEmail(employee.getEmail());
+		if(employeeOptional.isPresent()){
+			throw new IllegalArgumentException(
+					"Email " + employee.getEmail() + "is taken"
+			);
+		}
+		employeeRepository.saveAndFlush(employee);
+	}
 }
