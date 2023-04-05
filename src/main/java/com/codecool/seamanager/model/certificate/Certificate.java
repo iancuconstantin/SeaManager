7e53567ab0756a7893a2f29ca2c12b8f06179bb5
claudiumt <claudium.tudor@gmail.com>
2023-04-05 22:34:43 +0300

    Implement Certificate model class

--- src/main/java/com/codecool/seamanager/model/certificate/Certificate.java
+++ src/main/java/com/codecool/seamanager/model/certificate/Certificate.java
@@ -1,71 +1,117 @@
 package com.codecool.seamanager.model.certificate;
 
-import java.util.Objects;
+import com.codecool.seamanager.model.employee.Employee;
+import jakarta.persistence.*;
 
+@Entity
+@Table
 public class Certificate {
-	private final String ownerID;
-	private final String description;
-	private final String serialNumber;
-	private String issueDate;
-	private String expiryDate;
+	@Id
+	@GeneratedValue(
+			strategy = GenerationType.AUTO
+	)
+	@Column(
+			name = "certificate_id",
+			updatable = false
+	)
+	private Long certificateId;
+	@ManyToOne
+	@JoinColumn(
+			name = "employee_id",
+			referencedColumnName = "employee_id",
+			nullable = false,
+			columnDefinition = "BIGINT"
+	)
+	private Employee a_owner;
+	@Column(
+			name = "description",
+			nullable = false,
+			columnDefinition = "TEXT"
+	)
+	private String b_description;
+	@Column(
+			name = "serial_number",
+			nullable = false,
+			columnDefinition = "TEXT"
+	)
+	private String c_serialNumber;
+	@Column(
+			name = "issue_date",
+			nullable = false,
+			columnDefinition = "TEXT"
+	)
+	private String d_issueDate;
+	@Column(
+			name = "expiry_date",
+			columnDefinition = "TEXT"
+	)
+	private String e_expiryDate;
 
-	public Certificate(String ownerID, String description, String serialNumber, String issueDate, String expiryDate) {
-		this.ownerID = ownerID;
-		this.description = description;
-		this.serialNumber = serialNumber;
-		this.issueDate = issueDate;
-		this.expiryDate = expiryDate;
+	public Certificate() {
 	}
 
-	public String getOwnerID() {
-		return ownerID;
+	public Certificate(Employee owner, String description, String serialNumber, String issueDate, String expiryDate) {
+		this.a_owner = owner;
+		this.b_description = description;
+		this.c_serialNumber = serialNumber;
+		this.d_issueDate = issueDate;
+		this.e_expiryDate = expiryDate;
+		owner.addNewCertificate(this);
+	}
+
+	public Long getCertificateId() {
+		return certificateId;
+	}
+
+	public Employee getOwner() {
+		return a_owner;
 	}
 
 	public String getDescription() {
-		return description;
+		return b_description;
 	}
 
 	public String getIssueDate() {
-		return issueDate;
+		return d_issueDate;
 	}
 
-	public void setIssueDate(String issueDate) {
-		this.issueDate = issueDate;
+	public void setD_issueDate(String issueDate) {
+		this.d_issueDate = issueDate;
 	}
 
 	public String getExpiryDate() {
-		return expiryDate;
+		return e_expiryDate;
 	}
 
-	public void setExpiryDate(String expiryDate) {
-		this.expiryDate = expiryDate;
+	public void setE_expiryDate(String expiryDate) {
+		this.e_expiryDate = expiryDate;
 	}
 
 	public String getSerialNumber() {
-		return serialNumber;
+		return c_serialNumber;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Certificate that = (Certificate) o;
-		return ownerID.equals(that.ownerID) && description.equals(that.description) && serialNumber.equals(that.serialNumber) && issueDate.equals(that.issueDate) && Objects.equals(expiryDate, that.expiryDate);
+	public void setA_owner(Employee owner) {
+		this.a_owner = owner;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(ownerID, description, serialNumber, issueDate, expiryDate);
+	public void setB_description(String description) {
+		this.b_description = description;
+	}
+
+	public void setC_serialNumber(String serialNumber) {
+		this.c_serialNumber = serialNumber;
 	}
 
 	@Override
 	public String toString() {
 		return "Certificate{" +
-				"ownerID=" + ownerID +
-				", description='" + description + '\'' +
-				", serialNumber=" + serialNumber +
-				", issueDate='" + issueDate + '\'' +
-				", expiryDate='" + expiryDate + '\'' +
+				"id=" + certificateId +
+				", ownerId=" + a_owner +
+				", description='" + b_description + '\'' +
+				", serialNumber='" + c_serialNumber + '\'' +
+				", issueDate='" + d_issueDate + '\'' +
+				", expiryDate='" + e_expiryDate + '\'' +
 				'}';
 	}
 }
