09d1e6774f4baa95e0701fd706dc6b1c2f5a7af8
claudiumt <claudium.tudor@gmail.com>
2023-04-07 00:09:54 +0300

    Implement tests for CertificateController using Mockito + JUnit (WIP: shouldUpdateCertificate() )

---
+++ src/test/java/com/codecool/seamanager/controller/CertificateControllerTest.java
@@ -0,0 +1,139 @@
+package com.codecool.seamanager.controller;
+
+import com.codecool.seamanager.model.certificate.Certificate;
+import com.codecool.seamanager.model.employee.Employee;
+import com.codecool.seamanager.service.CertificateService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static com.codecool.seamanager.model.employee.Gender.MALE;
+import static com.codecool.seamanager.model.employee.Rank.THIRD_ENGINEER;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CertificateControllerTest {
+
+	private static final String END_POINT_PATH = "/api/certificate";
+	private Certificate certificate;
+	private Employee employee;
+	private MockMvc mockMvc;
+	@Mock
+	private CertificateService certificateService;
+	@InjectMocks
+	private CertificateController certificateController;
+
+	@Before
+	public void setUp(){
+		employee = new Employee(
+				"John",
+				"Doe",
+				"01-01-1990",
+				"+123456789",
+				"123 Main St",
+				"johndoe@gmail.com",
+				THIRD_ENGINEER,
+				MALE
+		);
+		employee.setEmployeeId(1L);
+
+		certificate = new Certificate(
+				employee,
+				"Seaman's Book",
+				"49681DD",
+				"11-01-2023",
+				"11-01-2029"
+		);
+
+		mockMvc = MockMvcBuilders.standaloneSetup(certificateController).build();
+	}
+
+	@Test
+	public void shouldReturnListOfCertificates() throws Exception {
+		List<Certificate> certificates = Arrays.asList(certificate);
+		when(certificateService.getCertificates()).thenReturn(certificates);
+
+		mockMvc.perform(get(END_POINT_PATH))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(APPLICATION_JSON_VALUE))
+				.andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].owner.employeeId").value(employee.getEmployeeId()))
+				.andExpect(jsonPath("$[0].description").value(certificate.getDescription()))
+				.andExpect(jsonPath("$[0].serialNumber").value(certificate.getSerialNumber()))
+				.andExpect(jsonPath("$[0].issueDate").value(certificate.getIssueDate()))
+				.andExpect(jsonPath("$[0].expiryDate").value(certificate.getExpiryDate()));
+	}
+
+	@Test
+	public void shouldReturnCertificateById() throws Exception{
+		Long certificateId = 1L;
+		when(certificateService.getCertificateById(certificateId)).thenReturn(certificate);
+
+		//TODO - handle checking id without id setter, if possible
+		mockMvc.perform(get(END_POINT_PATH + "/" + certificateId))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(APPLICATION_JSON_VALUE))
+				.andExpect(jsonPath("$.owner.employeeId").value(employee.getEmployeeId()))
+				.andExpect(jsonPath("$.description").value(certificate.getDescription()))
+				.andExpect(jsonPath("$.serialNumber").value(certificate.getSerialNumber()))
+				.andExpect(jsonPath("$.issueDate").value(certificate.getIssueDate()))
+				.andExpect(jsonPath("$.expiryDate").value(certificate.getExpiryDate()));
+	}
+
+	@Test
+	public void shouldAddNewEmployee() throws Exception {
+		String certificateJson = new ObjectMapper().writeValueAsString(certificate);
+
+		mockMvc.perform(post(END_POINT_PATH).contentType(APPLICATION_JSON).content(certificateJson))
+				.andExpect(status().isOk());
+
+		verify(certificateService, times(1)).addNewCertificate(any(Certificate.class));
+	}
+
+	@Test
+	public void shouldDeleteCertificateById() throws Exception {
+		Long certificateId = 1L;
+
+		mockMvc.perform(delete(END_POINT_PATH + "/" + certificateId))
+				.andExpect(status().isOk());
+
+		verify(certificateService, times(1)).deleteCertificate(certificateId);
+	}
+
+	@Test
+	public void shouldUpdateCertificate() throws Exception {
+		// Prepare test data
+		Long certificateId = 1L;
+		Certificate updatedCertificate = new Certificate(
+				employee,
+				"Updated Certificate",
+				"12345",
+				"01-01-2023",
+				"01-01-2029"
+		);
+
+		when(certificateService.updateCertificate(eq(certificateId), any(Certificate.class))).thenReturn(ResponseEntity.of(Optional.of(updatedCertificate)));
+
+		String updatedCertificateJson = new ObjectMapper().writeValueAsString(updatedCertificate);
+		mockMvc.perform(put(END_POINT_PATH + "/" + certificateId).contentType(APPLICATION_JSON).content(updatedCertificateJson))
+				.andExpect(status().isOk());
+				//TODO - add andExpects (breaks at "$.owner.employeeId")
+		verify(certificateService, times(1)).updateCertificate(eq(certificateId), any(Certificate.class));
+	}
+}
