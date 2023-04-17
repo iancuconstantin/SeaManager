cf82aec22f805604b78315b0241b80b982f75f2c
claudiumt <claudium.tudor@gmail.com>
2023-04-17 16:59:31 +0300

    Fix custom exceptions

--- src/main/java/com/codecool/seamanager/exceptions/certificate/CertificateSerialNumberDuplicationException.java
+++ src/main/java/com/codecool/seamanager/exceptions/certificate/CertificateSerialNumberDuplicationException.java
@@ -1,6 +1,11 @@
 package com.codecool.seamanager.exceptions.certificate;
 
-public class CertificateSerialNumberDuplicationException extends IllegalArgumentException {
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class CertificateSerialNumberDuplicationException extends DuplicateKeyException {
 	public CertificateSerialNumberDuplicationException(String message) {
 		super(message);
 	}
--- src/main/java/com/codecool/seamanager/exceptions/email/EmailTakenException.java
+++ src/main/java/com/codecool/seamanager/exceptions/email/EmailTakenException.java
@@ -1,6 +1,11 @@
 package com.codecool.seamanager.exceptions.email;
 
-public class EmailTakenException extends IllegalArgumentException{
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class EmailTakenException extends DuplicateKeyException {
 	public EmailTakenException(String message){
 		super(message);
 	}
