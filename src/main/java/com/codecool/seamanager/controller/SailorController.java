54d93647f181b999ca0999d07f96b197369bc05a
claudiumt <claudium.tudor@gmail.com>
2023-04-17 17:16:26 +0300

    Add Valid annotation on request bodies + change entity name

---
+++ src/main/java/com/codecool/seamanager/controller/SailorController.java
@@ -0,0 +1,47 @@
+package com.codecool.seamanager.controller;
+
+import com.codecool.seamanager.model.employee.Sailor;
+import com.codecool.seamanager.service.SailorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/employee")
+public class SailorController {
+
+	private final SailorService sailorService;
+
+	@Autowired
+	public SailorController(SailorService sailorService) {
+		this.sailorService = sailorService;
+	}
+
+	@GetMapping
+	public List<Sailor> getEmployees() {
+		return sailorService.getEmployees();
+	}
+
+	@GetMapping(path = "/{employeeId}")
+	public Sailor getEmployeeById(@PathVariable("employeeId") Long employeeId){
+		return sailorService.getEmployeeById(employeeId);
+	}
+
+	@PostMapping()
+	public void registerNewEmployee(@RequestBody Sailor sailor) {
+		sailorService.addNewEmployee(sailor);
+	}
+
+	@DeleteMapping(path = "{employeeId}")
+	public void deleteEmployee(@PathVariable("employeeId") Long employeeId){
+		sailorService.deleteEmployee(employeeId);
+	}
+
+	@PutMapping(path = "{employeeId}")
+	public ResponseEntity<Sailor> updateEmployee(@PathVariable Long employeeId, @RequestBody @Valid Sailor sailorDetails){
+		return sailorService.updateEmployee(employeeId, sailorDetails);
+	}
+}
