b26b2856b1e0777691192f3cfc7534094f66051b
claudiumt <claudium.tudor@gmail.com>
2023-04-18 17:17:14 +0300

    Add new entity PortInteraction and its repo + new enum

---
+++ src/main/java/com/codecool/seamanager/repository/PortInteractionRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.seamanager.repository;
+
+import com.codecool.seamanager.model.portinteraction.PortInteraction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortInteractionRepository extends JpaRepository<PortInteraction, Long> {
+}
