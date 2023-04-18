a55f21689a54febcee08aef0cb0ec17311becfe8
claudiumt <claudium.tudor@gmail.com>
2023-04-18 17:14:20 +0300

    Refactor Vessel entity and service/controller

--- src/main/java/com/codecool/seamanager/model/vessel/Vessel.java
+++ src/main/java/com/codecool/seamanager/model/vessel/Vessel.java
@@ -1,172 +1,62 @@
 package com.codecool.seamanager.model.vessel;
 
-import com.codecool.seamanager.model.employee.Sailor;
+import com.codecool.seamanager.model.voyage.Voyage;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @Entity
-@Table(name = "vessels")
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Vessel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@NotNull
-	@Column(name = "name")
+	@Column(name = "vessel_id")
+	private Long vesselId;
+	@NotBlank(message = "Vessel name should not be empty.")
+	@Column(columnDefinition = "TEXT")
 	private String name;
 	@NotNull
-	@Column(name = "type")
+	@Column(columnDefinition = "TEXT")
 	@Enumerated(EnumType.STRING)
 	private VesselType type;
-	@NotNull
+	@NotBlank(message = "Vessel flag should not be empty.")
 	@Column(name = "flag")
 	private String flag;
+
+	@Pattern(
+			regexp = "\\d{7}",
+			message = "Invalid IMO Number format. Should be 7 digits long."
+	)
+	@Column(
+			columnDefinition = "TEXT",
+			name = "IMO_number"
+	)
+	private String IMONumber;
 	@NotNull
-	@Column(name = "imonumber")
-	private long IMONumber;
-	@NotNull
-	@Column(name = "crewList")
 	@OneToMany(
-			cascade = CascadeType.ALL,
-			orphanRemoval = true,
+			targetEntity = Voyage.class,
 			mappedBy = "vessel"
 	)
-//	@JoinColumn(name = "vessel_id")
-	private List<Sailor> crewList;
+	@JsonIgnore
+	private Set<Voyage> voyages;
 
-	@NotNull
-	@Column(name = "nextPortOfCall")
-	private String nextPortOfCall;
-
-	public Vessel() {}
-
-	public Vessel(@NotNull String name, @NotNull VesselType type, @NotNull String flag, @NotNull long IMONumber, @NotNull String nextPortOfCall) {
+	public Vessel(String name, VesselType type, String flag, String IMONumber, Set<Voyage> voyages){
 		this.name = name;
 		this.type = type;
-		this.flag = flag;
+		this.flag= flag;
 		this.IMONumber = IMONumber;
-		this.crewList = new ArrayList<>();
-		this.nextPortOfCall = nextPortOfCall;
-	}
-
-
-
-
-	public void changeCrew(Sailor offSigner, Sailor onSigner) {
-		if (!offSigner.getRank().equals(onSigner.getRank())) {
-			throw new IllegalArgumentException(
-					"Off-signer: " + offSigner.getLastName() + ", rank: " + offSigner.getRank()
-							+ "doesn't match rank with on-signer: " + onSigner.getLastName() + ", rank: " + onSigner.getRank()
-			);
-		} else if (!isPartOfCrewList(offSigner)) {
-			throw new IllegalArgumentException(
-					"Off-signer " + offSigner.getLastName() + " is not part of of vessel's " + name + " crew list."
-			);
-		}
-
-		addCrew(onSigner);
-		removeCrew(offSigner);
-	}
-
-	public void removeCrew(Sailor sailor) {
-		if (isPartOfCrewList(sailor)) {
-			crewList.remove(sailor);
-		} else {
-			throw new IllegalArgumentException(
-					"Employee " + sailor.getLastName() + " is not part of of vessel's " + name + " crew list."
-			);
-		}
-	}
-
-
-	public void addCrew(Sailor sailor) {
-		crewList.add(sailor);
-	}
-
-	public Sailor getCrewMemberById(String employeeID){
-		for (Sailor sailor : crewList){
-			if(sailor.getEmployeeId().equals(employeeID)){
-				return sailor;
-			}
-		}
-		throw new IllegalArgumentException(
-				"Employee id: " + employeeID + " is not part of of vessel's " + name + " crew list."
-		);
-	}
-
-	private boolean isPartOfCrewList(Sailor sailor) {
-		for (Sailor e : crewList) {
-			if (e.getEmployeeId().equals(sailor.getEmployeeId())) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public VesselType getType() {
-		return type;
-	}
-
-
-	public void setType(VesselType type) {
-		this.type = type;
-	}
-
-	public String getFlag() {
-		return flag;
-	}
-
-
-	public void setFlag(String flag) {
-		this.flag = flag;
-	}
-
-
-	public long getIMONumber() {
-		return IMONumber;
-	}
-
-
-	public void setIMONumber(long IMONumber) {
-		this.IMONumber = IMONumber;
-	}
-
-
-	public List<Sailor> getCrewList() {
-		return crewList;
-	}
-
-
-	public void setCrewList(List<Sailor> crewList) {
-		this.crewList = crewList;
-	}
-
-
-	public String getNextPortOfCall() {
-		return nextPortOfCall;
-	}
-
-	public void setNextPortOfCall(String nextPortOfCall) {
-		this.nextPortOfCall = nextPortOfCall;
+		this.voyages = voyages;
 	}
 }
--- src/main/java/com/codecool/seamanager/service/VesselService.java
+++ src/main/java/com/codecool/seamanager/service/VesselService.java
@@ -1,11 +1,9 @@
 package com.codecool.seamanager.service;
 
-import com.codecool.seamanager.model.employee.Sailor;
 import com.codecool.seamanager.model.vessel.Vessel;
-import com.codecool.seamanager.model.vessel.VesselType;
 import com.codecool.seamanager.repository.SailorRepository;
 import com.codecool.seamanager.repository.VesselRepository;
-import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,72 +12,56 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class VesselService {
-    @Autowired
-    private VesselRepository vesselRepository;
-    @Autowired
-    private SailorRepository sailorRepository;
+	private final VesselRepository vesselRepository;
+	private final SailorRepository sailorRepository;
+
+	@Autowired
+	public VesselService(VesselRepository vesselRepository, SailorRepository sailorRepository) {
+		this.vesselRepository = vesselRepository;
+		this.sailorRepository = sailorRepository;
+	}
+
+	public List<Vessel> getAllVessels() {
+		return vesselRepository.findAll();
+	}
+
+	public Vessel getById(@PathVariable Long id) {
+		return vesselRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException(
+						"Vessel with id: " + id + " not found.")
+				);
+	}
+
+	public ResponseEntity<Vessel> createVessel(@RequestBody @Valid Vessel vessel) {
+		Vessel savedVessel = vesselRepository.save(vessel);
+		return ResponseEntity.created(URI.create("/vessel/" + savedVessel.getVesselId())).body(savedVessel);
+	}
 
 
-    public List<Vessel> getAllVessels(){return vesselRepository.findAll();}
+	public ResponseEntity<Vessel> updateVessel(@PathVariable Long id, @RequestBody @Valid Vessel vessel) {
+		Vessel vesselToUpdate = vesselRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException(
+						"Vessel with id:" + id + "not found.")
+				);
 
-    public ResponseEntity<Vessel> getById(@PathVariable Long id) {
-        Optional<Vessel> optionalVessel = vesselRepository.findById(id);
-        if (optionalVessel.isPresent()) {
-            Vessel vessel = optionalVessel.get();
-            return ResponseEntity.ok(vessel);
-        } else {
-            throw new EntityNotFoundException("Vessel not found with id: " + id);
-        }
-    }
-    public ResponseEntity<Vessel> createVessel(@RequestBody Vessel vessel) {
-        Vessel savedVessel = vesselRepository.save(vessel);
-        return ResponseEntity.created(URI.create("/vessel/" + savedVessel.getId())).body(savedVessel);
-    }
+		vesselToUpdate.setName(vessel.getName());
+		vesselToUpdate.setType(vessel.getType());
+		vesselToUpdate.setFlag(vessel.getFlag());
+		vesselToUpdate.setIMONumber(vessel.getIMONumber());
 
+		Vessel updatedVessel = vesselRepository.save(vesselToUpdate);
+		return ResponseEntity.ok(updatedVessel);
+	}
 
-    public ResponseEntity<Vessel> updateVessel(@PathVariable Long id, @RequestBody Vessel vessel) {
-        Optional<Vessel> optionalVessel = vesselRepository.findById(id);
-        if (optionalVessel.isPresent()) {
-            Vessel existingVessel = optionalVessel.get();
-            if(vessel.getName().length()>0) existingVessel.setName(vessel.getName());
-            if(vessel.getType() != null && vessel.getType() instanceof VesselType) existingVessel.setType(vessel.getType());
-            if(vessel.getFlag()!= null) existingVessel.setFlag(vessel.getFlag());
-            if(vessel.getIMONumber()!=-1) existingVessel.setIMONumber(vessel.getIMONumber());
-            if(vessel.getNextPortOfCall()!= null) existingVessel.setNextPortOfCall(vessel.getNextPortOfCall());
-            Vessel updatedVessel = vesselRepository.save(existingVessel);
-            return ResponseEntity.ok(updatedVessel);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	public void deleteVessel(@PathVariable Long id) {
+		boolean exists = vesselRepository.existsById(id);
+		if (!exists) {
+			throw new IllegalArgumentException("Vessel with id: " + id + " not found.");
+		}
 
-    public ResponseEntity<Void> deleteVessel(@PathVariable Long id) {
-        Optional<Vessel> optionalVessel = vesselRepository.findById(id);
-        if (optionalVessel.isPresent()) {
-            vesselRepository.deleteById(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    public void addEmployeeOnCrew(@PathVariable Long vesselId, @PathVariable Long employeeId){
-        Optional<Vessel> optionalVessel = vesselRepository.findById(vesselId);
-        Optional<Sailor> optionalEmployee = sailorRepository.findById(employeeId);
-
-        if(optionalVessel.isPresent() && optionalEmployee.isPresent()) {
-            Vessel existingVessel = optionalVessel.get();
-            Sailor existingSailor = optionalEmployee.get();
-            existingVessel.addCrew(existingSailor);
-            vesselRepository.save(existingVessel);
-            existingSailor.setVessel(existingVessel);
-            sailorRepository.save(existingSailor);
-            System.out.println("vvvvvvvvvvvvvvvvvvvvvvvvvvvvv "+existingVessel.getCrewList().size());
-//            return ResponseEntity.ok(existingVessel);
-        }
-    }
+		vesselRepository.deleteById(id);
+	}
 }
