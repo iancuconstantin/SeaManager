924c8c5a281aa5498aca7b574d11e75c9ce4abcb
claudiumt <claudium.tudor@gmail.com>
2023-04-19 14:33:12 +0300

    Fix CRUD logic in Vessel service

--- src/main/java/com/codecool/seamanager/service/VesselService.java
+++ src/main/java/com/codecool/seamanager/service/VesselService.java
@@ -1,5 +1,7 @@
 package com.codecool.seamanager.service;
 
+import com.codecool.seamanager.exceptions.vessel.IMONumberExistsException;
+import com.codecool.seamanager.exceptions.vessel.VesselNotFoundException;
 import com.codecool.seamanager.model.vessel.Vessel;
 import com.codecool.seamanager.repository.SailorRepository;
 import com.codecool.seamanager.repository.VesselRepository;
@@ -10,8 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VesselService {
@@ -30,27 +32,47 @@ public class VesselService {
 
 	public Vessel getById(@PathVariable Long id) {
 		return vesselRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException(
+				.orElseThrow(() -> new VesselNotFoundException(
 						"Vessel with id: " + id + " not found.")
 				);
 	}
 
-	public ResponseEntity<Vessel> createVessel(@RequestBody @Valid Vessel vessel) {
-		Vessel savedVessel = vesselRepository.save(vessel);
-		return ResponseEntity.created(URI.create("/vessel/" + savedVessel.getVesselId())).body(savedVessel);
+	public void createVessel(@RequestBody @Valid Vessel vessel) {
+		Optional<Vessel> vesselOptional = vesselRepository.findVesselByIMONumber(vessel.getIMONumber());
+
+		if (vesselOptional.isPresent()) {
+			throw new IMONumberExistsException(
+					"Vessel with IMO Number: " + vessel.getIMONumber() + " already exists."
+			);
+		}
+
+		vesselRepository.save(vessel);
 	}
 
 
 	public ResponseEntity<Vessel> updateVessel(@PathVariable Long id, @RequestBody @Valid Vessel vessel) {
 		Vessel vesselToUpdate = vesselRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException(
+				.orElseThrow(() -> new VesselNotFoundException(
 						"Vessel with id:" + id + "not found.")
 				);
+
+		Optional<Vessel> vesselOptional = vesselRepository.findVesselByIMONumber(vessel.getIMONumber());
+		if(vesselOptional.isPresent()){
+			throw new IMONumberExistsException(
+					"IMO number " + vessel.getIMONumber() + " exists in database."
+			);
+		}
+		vesselToUpdate.setIMONumber(vessel.getIMONumber());
 
 		vesselToUpdate.setName(vessel.getName());
 		vesselToUpdate.setType(vessel.getType());
 		vesselToUpdate.setFlag(vessel.getFlag());
-		vesselToUpdate.setIMONumber(vessel.getIMONumber());
+		vesselToUpdate.setEmail(vessel.getEmail());
+		vesselToUpdate.setDWT(vessel.getDWT());
+		vesselToUpdate.setGRT(vessel.getGRT());
+		vesselToUpdate.setCallSign(vessel.getCallSign());
+		vesselToUpdate.setYearBuilt(vessel.getYearBuilt());
+		vesselToUpdate.setMMSI(vessel.getMMSI());
 
 		Vessel updatedVessel = vesselRepository.save(vesselToUpdate);
 		return ResponseEntity.ok(updatedVessel);
@@ -59,7 +81,7 @@ public class VesselService {
 	public void deleteVessel(@PathVariable Long id) {
 		boolean exists = vesselRepository.existsById(id);
 		if (!exists) {
-			throw new IllegalArgumentException("Vessel with id: " + id + " not found.");
+			throw new VesselNotFoundException("Vessel with id: " + id + " not found.");
 		}
 
 		vesselRepository.deleteById(id);
