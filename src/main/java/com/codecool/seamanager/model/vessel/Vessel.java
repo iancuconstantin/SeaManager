66775688a484b7c41caa98e84028a53507e515df
claudiumt <claudium.tudor@gmail.com>
2023-04-19 14:30:40 +0300

    Add new fields for Vessel entity and their validations

--- src/main/java/com/codecool/seamanager/model/vessel/Vessel.java
+++ src/main/java/com/codecool/seamanager/model/vessel/Vessel.java
@@ -3,6 +3,7 @@ package com.codecool.seamanager.model.vessel;
 import com.codecool.seamanager.model.voyage.Voyage;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -32,9 +34,40 @@ public class Vessel {
 	@Enumerated(EnumType.STRING)
 	private VesselType type;
 	@NotBlank(message = "Vessel flag should not be empty.")
-	@Column(name = "flag")
+	@Pattern(
+			regexp = "[a-zA-Z]*",
+			message = "Vessel's flag should have only letters."
+	)
 	private String flag;
-
+	@NotBlank(message = "MMSI number should not be empty.")
+	@Pattern(
+			regexp = "\\d{9}",
+			message = "MMSI number should be 9 digits long."
+	)
+	private String MMSI;
+	@NotBlank(message = "Call Sign should not be empty.")
+	@Pattern(
+			regexp = "[a-zA-Z\\d]{5}",
+			message = "Call Sign should be 5 characters long and contain only digits and letters."
+	)
+	private String callSign;
+	@NotBlank(message = "GRT should not be empty.")
+	@Pattern(
+			regexp = "[\\d]{5,6}",
+			message = "Incorrect GRT format. Accepting only digits (min 5, max 6)."
+	)
+	private String GRT;
+	@NotBlank(message = "DWT should not be empty.")
+	@Pattern(
+			regexp = "[\\d]{5,6}",
+			message = "Incorrect DWT format. Accepting only digits (min 5, max 6)."
+	)
+	private String DWT;
+	@NotBlank(message = "Year built should not be empty.")
+	@Pattern(regexp = "\\d{4}", message = "Year built must be a 4-digit number")
+	private String yearBuilt;
+	@Email(message = "Not a valid email.")
+	private String email;
 	@Pattern(
 			regexp = "\\d{7}",
 			message = "Invalid IMO Number format. Should be 7 digits long."
@@ -50,13 +83,18 @@ public class Vessel {
 			mappedBy = "vessel"
 	)
 	@JsonIgnore
-	private Set<Voyage> voyages;
+	private Set<Voyage> voyages = new HashSet<>();
 
-	public Vessel(String name, VesselType type, String flag, String IMONumber, Set<Voyage> voyages){
+	public Vessel(String name, VesselType type, String flag, String MMSI, String email, String callSign, String IMONumber, String yearBuilt, String DWT, String GRT) {
 		this.name = name;
 		this.type = type;
-		this.flag= flag;
+		this.flag = flag;
 		this.IMONumber = IMONumber;
-		this.voyages = voyages;
+		this.callSign = callSign;
+		this.yearBuilt = yearBuilt;
+		this.email = email;
+		this.MMSI = MMSI;
+		this.DWT = DWT;
+		this.GRT = GRT;
 	}
 }
