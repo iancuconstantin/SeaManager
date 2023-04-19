ec9041b04577e0d3c0c2bc39ab9c9270cb5e6563
claudiumt <claudium.tudor@gmail.com>
2023-04-19 14:34:11 +0300

    Add new query helper method

--- src/main/java/com/codecool/seamanager/repository/VesselRepository.java
+++ src/main/java/com/codecool/seamanager/repository/VesselRepository.java
@@ -2,6 +2,12 @@ package com.codecool.seamanager.repository;
 
 import com.codecool.seamanager.model.vessel.Vessel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface VesselRepository extends JpaRepository<Vessel, Long> {
+
+	@Query("SELECT v FROM Vessel v WHERE v.IMONumber = ?1")
+	Optional<Vessel> findVesselByIMONumber(String IMONumber);
 }
