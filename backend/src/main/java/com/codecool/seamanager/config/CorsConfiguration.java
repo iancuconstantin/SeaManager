563a7ac5f00f15f87d6218a5c6d6062ec8e13931
claudiumt <claudium.tudor@gmail.com>
2023-05-15 23:05:32 +0300

    Configured CORS and Basic Authentication in SecurityConfig

--- backend/src/main/java/com/codecool/seamanager/config/CorsConfiguration.java
+++ backend/src/main/java/com/codecool/seamanager/config/CorsConfiguration.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
 
 }
