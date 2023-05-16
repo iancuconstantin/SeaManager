ae2da00b83b25fdbb956b11a05f402f32e40e16a
claudiumt <claudium.tudor@gmail.com>
2023-05-16 13:45:45 +0300

    Implement authentication using InMemoryUserDetails with jwt

--- backend/src/main/java/com/codecool/seamanager/SeamanagerApplication.java
+++ backend/src/main/java/com/codecool/seamanager/SeamanagerApplication.java
@@ -1,10 +1,12 @@
 package com.codecool.seamanager;
 
+import com.codecool.seamanager.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-//@EnableJpaRepositories
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SeamanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SeamanagerApplication.class, args);
