015f3c4772e36e8e1850c6ee7d61f3a6a385c81c
claudiumt <claudium.tudor@gmail.com>
2023-05-19 00:52:41 +0300

    fix security config to allow cross-origin requests

--- backend/src/main/java/com/codecool/seamanager/config/SecurityConfig.java
+++ backend/src/main/java/com/codecool/seamanager/config/SecurityConfig.java
@@ -31,6 +31,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -88,7 +89,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
-		configuration.setAllowedHeaders(List.of("Authorization"));
+		configuration.setAllowedHeaders(Collections.singletonList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
